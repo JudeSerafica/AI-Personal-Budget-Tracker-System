14a8c52479c06530ce79f271024e466b5c5fdb70
JudeSerafica <202312280@gordoncollege.edu.ph>
2025-11-26 12:49:14 +0800

    initial commit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -129,7 +129,7 @@ export default function LoginPage() {
             <div className="absolute inset-0 flex items-center">
               <div className="w-full border-t border-gray-300" />
             </div>
-          </div>n
+          </div>
         </div>
       </div>
     </div>
